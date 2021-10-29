b851780340989cd744b2c1b727804284220900ac
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-10-28 21:31:43 -0300

    Criação das API´s do projeto

---
+++ src/building_blocks/GWS.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GWS.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/web/GWS.WebApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GWS.WebApp.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
